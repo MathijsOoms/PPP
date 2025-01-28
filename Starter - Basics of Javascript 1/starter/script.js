eafefc803ba58da1cc80af774084aea22328e6ea
Mathijs Ooms <r0929441@student.thomasmore.be>
2025-01-28 20:11:51 +0100

    1-12

---
+++ Starter - Basics of Javascript 1/starter/script.js
@@ -0,0 +1,105 @@
+// let fruit = 'apple';
+
+// console.log('Michael');
+
+// let firstName = 'Michael';
+// console.log(firstName);
+// /*console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);*/
+
+// // Variable Name Conventions
+// let PI = 3.141592;
+
+// let firstJob = 'programmer';
+// let currentJob = 'teacher';
+
+// console.log(firstJob)
+
+// let appleIsFruit = true;
+// console.log(appleIsFruit)
+// console.log(typeof true)
+// console.log(typeof 'kjbv')
+// console.log(typeof 22)
+// console.log(typeof appleIsFruit)
+
+// appleIsFruit = 'true'
+// console.log(typeof appleIsFruit) //Dynamic typing
+
+// let _undefined = 26;
+// console.log(_undefined);
+// console.log(typeof _undefined);
+// console.log(typeof null);
+
+// let price = 120;
+// price = 250;
+
+// const birthYear = 1996;
+
+// console.log(birthYear);
+
+// // var category = 'electronics';
+// // category = 'clothes';
+
+// // console.log(category);
+
+// Arithmetic Operators
+
+// const currentYear = 2025;
+// const age1 = currentYear - 1996;
+// const age2 = currentYear - 2005;
+// console.log(age1, age2);
+
+// console.log(age1 * 16);
+// console.log(age1 / 10);
+// console.log(5 ** 4);
+
+// const firstName = 'John';
+// const lastName = 'Doe';
+// console.log(firstName + ' ' + lastName);
+
+// // Assignment operator
+// let fullName = firstName + lastName;
+// console.log(fullName);
+
+// let counter = 0;
+// counter += 10;
+// console.log(counter);
+
+// counter *= 5;
+// console.log(counter);
+
+// counter /= 2;
+// console.log(counter);
+
+// counter ++;
+// console.log(counter);
+
+// counter--;
+// counter--;
+// counter--;
+// console.log(counter)
+
+// // Comparison opperators
+
+// const legalAge = age2 >= 18;
+// console.log(age1 > age2);
+
+// console.log(legalAge);
+
+// console.log(currentYear - 1996 > currentYear - 2005);
+
+const currentYear = 2025;
+const age1 = currentYear - 1996;
+const age2 = currentYear - 2005;
+
+console.log(currentYear - 1996 > currentYear - 2005);
+console.log(100 - 30 - 10);
+
+let a, b;
+a = b = 100 - 30 - 10;
+console.log(a, b);
+
+console.log(age1, age2);
+const average = (age1 + age2) / 2;
+console.log(average);
