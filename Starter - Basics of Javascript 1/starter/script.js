0531a0da24ac5fff7e753826f0c9a21e682c7f75
Mathijs Ooms <r0929441@student.thomasmore.be>
2025-02-03 16:48:46 +0100

    13-21

--- Starter - Basics of Javascript 1/starter/script.js
+++ Starter - Basics of Javascript 1/starter/script.js
@@ -89,17 +89,158 @@
 
 // console.log(currentYear - 1996 > currentYear - 2005);
 
-const currentYear = 2025;
-const age1 = currentYear - 1996;
-const age2 = currentYear - 2005;
+// const currentYear = 2025;
+// const age1 = currentYear - 1996;
+// const age2 = currentYear - 2005;
 
-console.log(currentYear - 1996 > currentYear - 2005);
-console.log(100 - 30 - 10);
+// console.log(currentYear - 1996 > currentYear - 2005);
+// console.log(100 - 30 - 10);
 
-let a, b;
-a = b = 100 - 30 - 10;
-console.log(a, b);
+// let a, b;
+// a = b = 100 - 30 - 10;
+// console.log(a, b);
 
-console.log(age1, age2);
-const average = (age1 + age2) / 2;
-console.log(average);
+// console.log(age1, age2);
+// const average = (age1 + age2) / 2;
+// console.log(average);
+
+// const brandName = 'Toyota';
+// const modelName = 'Corolla';
+// const year = 2015;
+
+// const myCar = 'My car is ' + brandName + ' ' + modelName + ' bought in ' + year;
+
+// console.log(myCar);
+
+// const myCar2 = `My car is ${brandName} ${modelName}, I bought it in ${year}`;
+// console.log(myCar2);
+
+// console.log('hello \n how are you \n I am fine');
+// console.log(
+//     `Hello
+//         how are you
+//             i am fine`
+// );
+
+// const personAge = 20;
+// const isLegal = personAge >= 18;
+
+// if(isLegal) {
+//     console.log('John can buy alcohol')
+// } else {
+//     const yearsLeft = 18 - personAge;
+//     console.log(`John is too young for drinking, he has to wait ${yearsLeft} years before he can start drinking alcohol`);
+// }
+
+// // TYPE CONVERSION
+// const imputAge = '26';
+// console.log(Number(imputAge) + 3);
+// console.log(Number(imputAge), imputAge);
+// console.log(Number('hello'));
+// console.log(typeof NaN);
+// console.log(String(100), 100);
+
+// // TYPE COHERSION
+// console.log('I was born in ' + 1999);
+// console.log('1999' - '20' - 10);
+// console.log('1990' * '5');
+// let number = '10' + 10; //1010
+// number = number - 10; //1000
+// console.log(number);
+
+// Falsy values: 0, '', undefined, null, nan
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('ajeb'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const candy = 0;
+
+// if (candy) {
+//     console.log('Do not eat too much candy');
+// } else {
+//     console.log('You have no candy')
+// }
+
+// let age = 0; //undefined
+
+// if (age) {
+//     console.log('age');
+// } else {
+//     console.log('no age');
+// }
+
+// const age = '18';
+
+// if(age === 18) console.log('You are 18 years old (===)');
+// if(age == 18) console.log('You are 18 years old (==)');
+
+// const inputAge = Number(prompt('What is your age?'));
+// console.log(inputAge);
+
+// if (inputAge == 26) console.log('you are 26 years old')
+//     else if(inputAge === 12) console.log('you are 12 years old')
+// else console.log('you are not 26 or 12');
+
+// if(inputAge !== 26) console.log('No,no no');
+
+// const hasMotorcycle = true;
+// const hasCar = true;
+
+// console.log(hasMotorcycle && hasCar);
+// console.log(hasMotorcycle || hasCar);
+// console.log(!hasMotorcycle);
+
+// const isRainy = true;
+// const shouldDrive = hasCar && isRainy;
+
+// if (shouldDrive) {
+//     console.log('John should drive because rainy')
+// } else if (hasMotorcycle && !isRainy) {
+//     console.log('John should go with his motorcycle')
+// } else {
+//     console.log('You should not drive')
+// };
+
+// const operator = '+';
+
+// const number1 = 10;
+// const number2 = 5;
+
+// switch(operator) {
+//     case '+':
+//         console.log('addition');
+//         console.log(`${number1 + number2}`);
+//         break;
+//     case '-':
+//         console.log('addition');
+//         console.log(`${number1 - number2}`);
+//         break;
+//     case '*':
+//         console.log('addition');
+//         console.log(`${number1 * number2}`);
+//         break;
+//     case '/':
+//         console.log('addition');
+//         console.log(`${number1 / number2}`);
+//         break;
+//     default:
+//         console.log('Invalid operator')
+//         break;
+// }
+
+// EXPRESSIONS
+8 + 7;
+1000;
+true && false && !false;
+
+// STATEMENTS
+if (12 < 26) {
+    console.log('12 is smaller than 26')
+}
+
+
+const str = 'hgsdv';
+console.log(`${str} apples`)
